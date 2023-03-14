ef976622b688268703e6b0d447a09386c25dd24c
Rob Reiss <robreiss@hotmail.com>
2023-03-14 11:04:17 -0700

    pipe

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 'use strict';
 
 const express = require('express');
+const { Pool, Client } = require('pg')
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config()
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
+// const pool = new Pool({
+// 	host: 'db-01.chlk91o3lwqq.us-west-2.rds.amazonaws.com',
+// 	port: 5432,
+// 	user: 'fircrest',
+// 	database: 'fircrest',
+// 	password: process.env.PGPASSWORD,
+// 	ssl: {
+// 		ca: [fs.readFileSync(path.resolve('./rds-ca-2019-root.pem'), 'ascii')]
+// 	}
+// })
+
+async function pgget() {
+	// const { rows } = await pool.query('SELECT $1::text as message', ['Hello 12 Postgres!'])
+	const { rows } = await pool.query('SELECT * from test')
+	let msg = ""
+	for (let i = 0; i < rows.length; i++) {
+		msg += rows[i].id + " " + rows[i].name + "<br>"
+	}
+
+	return msg
+}
+
 const app = express();
-app.get('/', (req, res) => {
-	res.send('Hello World, deployed from done for night');
+app.get('/', async (req, res) => {
+	// let msg = await pgget()
+	let msg = "boo"
+	res.send('Hello World, deployed from done for night<br>' + msg);
 });
 
 app.listen(PORT, HOST, () => {
